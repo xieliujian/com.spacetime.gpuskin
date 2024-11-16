81aa4bcab80b2d7429d7ae13f8ca1ad653535467
xieliujian <377624289@qq.com>
2024-11-16 22:47:38 +0800

    1

--- Runtime/Scripts/GPUSkinMgr.cs
+++ Runtime/Scripts/GPUSkinMgr.cs
@@ -34,9 +34,42 @@ namespace ST.GPUSkin
         /// <summary>
         /// 
         /// </summary>
+        static readonly int GLOBAL_FRAME_INDEX = Shader.PropertyToID("g_GpuSkinFrameIndex");
+
+        /// <summary>
+        /// .
+        /// </summary>
+        const int FRAME_RATE = 30;
+
+        /// <summary>
+        /// .
+        /// </summary>
+        float m_LastFrameTime;
+
+        /// <summary>
+        /// 全局帧索引
+        /// </summary>
+        int m_GlobalFrameIndex;
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void OnUpdate()
         {
+            RefreshGlobalFrameIndex();
+        }
 
+        /// <summary>
+        /// 刷新shader的全局帧索引
+        /// </summary>
+        void RefreshGlobalFrameIndex()
+        {
+            float frameTime = 1.0f / FRAME_RATE;
+            if (Time.time - m_LastFrameTime >= frameTime)
+            {
+                m_LastFrameTime = Time.time;
+                Shader.SetGlobalInt(GLOBAL_FRAME_INDEX, m_GlobalFrameIndex++);
+            }
         }
     }
 }
